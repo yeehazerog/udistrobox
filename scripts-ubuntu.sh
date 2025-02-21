163fd838c8c0ea107067a9e198786bed2b6539d3
yeehazerog <yeehazerog@users.noreply.github.com>
2025-02-21 13:05:22 +0100

    Update scripts-ubuntu.sh

--- scripts-ubuntu.sh
+++ scripts-ubuntu.sh
@@ -17,42 +17,54 @@ fi
 # Change default shell to zsh
 if [[ "$SHELL" != *zsh* ]]; then
     echo "Changing default shell to zsh..."
-    chsh -s $(which zsh)
+    #chsh -s $(which zsh)
+    sudo chsh -s $(which zsh) root
+    sudo chsh -s $(which zsh) dev
 else
     echo "zsh is already the default shell."
 fi
 
-# Create .zshrc file if it doesn't exist
-ZSHRC_FILE="$HOME/.zshrc"
-if [ ! -f "$ZSHRC_FILE" ]; then
-    echo "Creating .zshrc file..."
-    touch "$ZSHRC_FILE"
-fi
+# # Create .zshrc file if it doesn't exist
+# ZSHRC_FILE="$HOME/.zshrc"
+# if [ ! -f "$ZSHRC_FILE" ]; then
+#     echo "Creating .zshrc file..."
+#     touch "$ZSHRC_FILE"
+# fi
 
-# Create .zprofile file if it doesn't exist
-ZPROFILE_FILE="$HOME/.zprofile"
-if [ ! -f "$ZPROFILE_FILE" ]; then
-    echo "Creating .zprofile file..."
-    touch "$ZPROFILE_FILE"
-fi
+# # Create .zprofile file if it doesn't exist
+# ZPROFILE_FILE="$HOME/.zprofile"
+# if [ ! -f "$ZPROFILE_FILE" ]; then
+#     echo "Creating .zprofile file..."
+#     touch "$ZPROFILE_FILE"
+# fi
 
-# Create .zlogin file if it doesn't exist
-ZLOGIN_FILE="$HOME/.zlogin"
-if [ ! -f "$ZLOGIN_FILE" ]; then
-    echo "Creating .zlogin file..."
-    touch "$ZLOGIN_FILE"
-fi
+# # Create .zlogin file if it doesn't exist
+# ZLOGIN_FILE="$HOME/.zlogin"
+# if [ ! -f "$ZLOGIN_FILE" ]; then
+#     echo "Creating .zlogin file..."
+#     touch "$ZLOGIN_FILE"
+# fi
 
-# Create .zlogout file if it doesn't exist
-ZLOGOUT_FILE="$HOME/.zlogout"
-if [ ! -f "$ZLOGOUT_FILE" ]; then
-    echo "Creating .zlogout file..."
-    touch "$ZLOGOUT_FILE"
-fi
+# # Create .zlogout file if it doesn't exist
+# ZLOGOUT_FILE="$HOME/.zlogout"
+# if [ ! -f "$ZLOGOUT_FILE" ]; then
+#     echo "Creating .zlogout file..."
+#     touch "$ZLOGOUT_FILE"
+# fi
 
-echo "Install oh my zsh"
+echo "Install oh my zsh for root"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+echo "Install oh my zsh for dev"
+sudo -u dev sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 echo "\ue0b0 \u00b1 \ue0a0 \u27a6 \u2718 \u26a1 \u2699"
+
+# Set ZSH_THEME to agnoster for 'dev' user
+if [ -f "$DEV_ZSHRC_FILE" ]; then
+  echo "Setting ZSH_THEME to agnoster for dev..."
+  sudo -u dev sed -i '' 's/^ZSH_THEME=.*/ZSH_THEME="agnoster"/' "$DEV_ZSHRC_FILE"
+else
+  echo "$DEV_ZSHRC_FILE does not exist."
+fi
 
 # Check if the .zshrc file exists
 if [ -f "$ZSHRC_FILE" ]; then
